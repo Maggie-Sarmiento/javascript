2bba659e070ef20e62cd41dbf51c995e675ef511
Maggie-Sarmiento <gsmaggie001@gmail.com>
2021-06-08 17:48:56 -0500

    javascript 2.1

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// esto es un comentario en javascript
+
+/* 
+este es un comentario de varias lineas
+como puedes ver
+en este 
+*/
+
+/**
+ * Este es un hack para escribir comentarios
+ */
+
+/**
+ * Tipos de Datos
+ */
+
+// Number
+
+console.log(typeof 50)
