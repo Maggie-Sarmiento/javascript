89bd9230b81af6438e9e8edfbf681318374c3524
Maggie-Sarmiento <gsmaggie001@gmail.com>
2021-06-08 18:13:59 -0500

    Class 2.2 Date type

--- scripts.js
+++ scripts.js
@@ -1,19 +1,26 @@
-// esto es un comentario en javascript
-
-/* 
-este es un comentario de varias lineas
-como puedes ver
-en este 
-*/
-
-/**
- * Este es un hack para escribir comentarios
- */
 
 /**
  * Tipos de Datos
  */
 
-// Number
+// NUMBER - Para todos los tipos de numeros. Enteros y decimales
 
-console.log(typeof 50)
+/**
+ * Console.log(typeof 25)
+ */
+
+// STRING - Cadena de texto
+
+/**
+ * console.log(typeof "Hola")
+ */
+
+// BOOLEAN - false, true
+
+/**
+ * console.log(typeof true)
+ */
+
+// console.log() : metodo y objeto de javascrip para imprimir en la consola
+
+// typeof : para leer un tipo de dato
