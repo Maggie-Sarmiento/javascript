de46f9e1b443c65d7227f10bca4bccd24cc54985
Maggie-Sarmiento <gsmaggie001@gmail.com>
2021-06-09 13:55:45 -0500

    Class 3.3 Operadores de concatenacion

--- scripts.js
+++ scripts.js
@@ -1,14 +1,28 @@
 
 /**
- * OPERADORES DE ASIGNACION
+ * OPERADORES ARITMETICOS DE CONCATENACION
  */
 
- // operador = para asignar un valor 
+ // operador +
 
-let a = 5
-let b = 3
+    let nombre = "Beto"
+    let apellido = "Quiroga"
 
-// a+= b // += suma y asigna el valor  // -= *= /= %=  
-// a**= b // potencia
+   // let nombreCompleto = nombre + " " + apellido
 
-console.log(a**b)
+  //  console.log(nombreCompleto)
+
+    // Template Strings
+
+let nickname = `Mi nombre es: ${nombre} y mi apellido es ${apellido}`  // dentro de los backs ticks se pueden escribir variables y operaciones
+// console.log(nickname) 
+
+let numero = 10
+let numero2 = 4
+let numero3 = 5
+
+suma = numero + numero2 + numero3
+let resultado = `La suma de ${numero + numero2} + ${numero3} es igual a: ${suma} `
+console.log(resultado)
+
+// ${20>10} = true usar los template string para concatenar
