bae8aaf8429c039fd2747a3a1f55c03aa0824365
Maggie-Sarmiento <gsmaggie001@gmail.com>
2021-06-09 14:13:12 -0500

    Class 3.4 Operadores de comparación I

--- scripts.js
+++ scripts.js
@@ -1,28 +1,30 @@
 
 /**
- * OPERADORES ARITMETICOS DE CONCATENACION
+ * OPERADORES DE COMPARACION
  */
 
- // operador +
+// ES IGUAL == (Compara solo el valor del contenido)
 
-    let nombre = "Beto"
-    let apellido = "Quiroga"
+let a = 50
+let b = "50"
 
-   // let nombreCompleto = nombre + " " + apellido
+let c= 6
 
-  //  console.log(nombreCompleto)
+// let resultado = a == b
 
-    // Template Strings
+// let resultado = a === b
 
-let nickname = `Mi nombre es: ${nombre} y mi apellido es ${apellido}`  // dentro de los backs ticks se pueden escribir variables y operaciones
-// console.log(nickname) 
+// let resultado = a != c
 
-let numero = 10
-let numero2 = 4
-let numero3 = 5
+let resultado = a !== b
 
-suma = numero + numero2 + numero3
-let resultado = `La suma de ${numero + numero2} + ${numero3} es igual a: ${suma} `
 console.log(resultado)
 
-// ${20>10} = true usar los template string para concatenar
+// ES ESTRICTAMENE IGUAL === (Compara valor y tipo de dato)
+
+// ES DIFERENTE != (Verifica que los valores sean diferentes)
+
+// ES ESTRICTAMENTE DIFERENTE !== (v)
+
+// Se puede usar con cadenas de texto let a = "Alvaro" let b = "Alvaro"
+// Se puede usar con booleanos let a = true let b = false
