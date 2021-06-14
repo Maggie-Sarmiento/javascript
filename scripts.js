f647c93261de02a8f875c946fd987bf38412ff64
Maggie-Sarmiento <gsmaggie001@gmail.com>
2021-06-14 11:29:35 -0500

    Class 4.1 Las condicionales

--- scripts.js
+++ scripts.js
@@ -1,29 +1,17 @@
 /**
- * TYPE COERCION  capacidad de convertir un dato en otro tipo de dato
+ * CONDICIONALES
  */
 
- let a = 521 
- let b = "422"
+ /**
+  * Invitación a una fiesta
+  * Si eres mayor de edad y menor de 65 años puedes venir a la fiesta
+  */
 
 
-//let resultado = a + b  //convierte todo a String por + (O aritmetico) que es de concatenacion
+let persona = "Juan Alexis"
+let edad = 65 // La condicional no se cumple 
 
-//let resultado = a - b
+if (edad > 18 && edad < 65) {
+   console.log(`${persona} puedes venir a la fiesta`)
+}
 
- // console.log(typeof resultado)
-
- let c = 521
- let d = "perro"
-
-//let resultado = c - d
-//console.log(resultado)
-
-// NaN => Not  number // Por el operador - lo reconoce como numero auqneu el resultado no lo sea
-
-let e = true
-let f = false
-
-let resultado = e + f
-console.log(resultado) // El operador + busca que sea cadena de texto, luego se da cuenta que quieres hacer una operacion
-
-// true vale 1 por eso de resultado da 2  los suma a peasar de que son booleanos
