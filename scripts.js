9b56eee34439d797598be82ae097cce24804cff4
Maggie-Sarmiento <gsmaggie001@gmail.com>
2021-06-14 12:17:35 -0500

    Class 4.4 Switch condicionales multiples

--- scripts.js
+++ scripts.js
@@ -1,27 +1,32 @@
 /**
- * CONDICIONALES
+ * CONDICIONALES MULTIPLES SWITCH
  */
 
  /**
-  * Invitación a una fiesta
-  * Si eres mayor de edad y menor de 65 años puedes venir a la fiesta
-  * sino, si tienes permiso firmado por tus padres, puedes venir
-  * sino por favor quedate en casa
+  * Que personaje de Dragon ball eres
+  * Si eres fuerte y comelon eres Goku
+  * Si eres veloz y egoista eres Vegueta
+  * Si eres pequeño y debil eres Krilin
+  * Si eres travieso y juegueton eres Trons
+  * Si no eres ninguno eres una sabandija
   */
 
 
-let persona = "Juan Alexis"
-let edad = 80 // La condicional no se cumple 
-let permiso = false
+let personalidad = "Travieso y gordo"
 
-if (edad > 18 && edad < 65) {
-   console.log(`${persona} puedes venir a la fiesta`)
-} else if (permiso === true && edad < 18) {
-    console.log(`Puedes venir ${persona} tienes permiso`)
-} else {
-    console.log(`${persona} quedate en casa`)
+switch (personalidad) {
+    case "Fuerte y comelón":
+        console.log("Eres Goku")
+        break
+    case "Veloz y egoista":
+        console.log("Eres Vegueta")
+        break
+    case "Pequeño y debil":
+        console.log("Eres Krilin")
+        break
+    case "Travieso y jugueton":
+        console.log("Eres Trons")
+        break
+    default:
+        console.log("Eres una sabandija")
 }
-
-// if
-// if else
-// if else if
