1bc4c5d1f955c05e6f0023e4f9a32aab283899ae
Maggie-Sarmiento <gsmaggie001@gmail.com>
2021-06-14 15:59:47 -0500

    Class 5.2 Parametros de función

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
 /**
- * FUNCIONES
+ * FUNCIONES - Parámetros
  */
 
 // Declaración de mi función saludar
 
-function saludar() {
-    console.log("Hola, muy buenos días")
+function saludar(nombre) {
+    if (typeof nombre === "string"){
+        console.log(`Hola ${nombre}, muy buenos días`)
+    } else {
+        console.log("Error de tipo de dato")
+    }
 }
 
+
+
 //Llamado a la función saludar
-saludar()
+saludar(550)
 
