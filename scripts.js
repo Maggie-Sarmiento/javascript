3556a190354c86c918c4edfd81e259d4def22cca
Maggie-Sarmiento <gsmaggie001@gmail.com>
2021-06-14 12:30:53 -0500

    Class 4.6  Ciclo for

--- scripts.js
+++ scripts.js
@@ -1,32 +1,17 @@
 /**
- * CONDICIONALES MULTIPLES SWITCH
+ * CICLOS
+ * BUCLES O LOOPS
  */
 
- /**
-  * Que personaje de Dragon ball eres
-  * Si eres fuerte y comelon eres Goku
-  * Si eres veloz y egoista eres Vegueta
-  * Si eres pequeño y debil eres Krilin
-  * Si eres travieso y juegueton eres Trons
-  * Si no eres ninguno eres una sabandija
-  */
-
-
-let personalidad = "Travieso y gordo"
-
-switch (personalidad) {
-    case "Fuerte y comelón":
-        console.log("Eres Goku")
-        break
-    case "Veloz y egoista":
-        console.log("Eres Vegueta")
-        break
-    case "Pequeño y debil":
-        console.log("Eres Krilin")
-        break
-    case "Travieso y jugueton":
-        console.log("Eres Trons")
-        break
-    default:
-        console.log("Eres una sabandija")
+//  for
+for (let i = 0; i < 10; i++) {
+    console.log(i)
 }
+
+/* 
+i vale 0; i es menor que 10? si = console.log(i), i se incrementa y ahora vale 1
+i vale 1, i es menor que 10? si = console.log(i), i se incrementa y ahora vale 2
+y asi hasta que y valga 10
+i vale 10, i es menor que 10? no TERMINE
+*/
+
