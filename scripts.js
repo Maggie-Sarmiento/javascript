1ff278cb4793aee10e17d62676f9976e4a44ecd5
Maggie-Sarmiento <gsmaggie001@gmail.com>
2021-06-15 14:36:55 -0500

    Class 6.3 forEach Array - Ciclos

--- scripts.js
+++ scripts.js
@@ -1,15 +1,32 @@
 /**
- * ARRAYS - Metodos que no modifican el array
+ * ARRAYS - Ciclos
  */
 
 // let amigos = []
 let amigos = ["Diana", "Roberto", "Valentina", "Susana"]
-console.log(amigos)
 
-// Partir nuestro arrays en dos arrays
-let dato = amigos.slice(0, 2)
+// console.log(amigos)
 
-console.log(dato)
-console.log(amigos)
+/* for (let i = 0; i < amigos.length; i++){
+    console.log(amigos[i])
+} */
 
-// Hay metodos que modifican el array (ej. push y pop), hay otros como slice que no los modifican y en su lugar nos devuelven un nuevo arreglo
+// los arrays ya traen metodos para hacer recorridos de una manera más optima
+// For each (para cada elemento) necesita como parametro una funcion
+
+/* amigos.forEach(function(amigo){
+    console.log(amigo)
+})
+ */
+
+amigos.forEach(amigo => console.log(amigo))
+
+/* let dato = amigos.forEach(amigo => console.log(amigo))
+console.log(dato) */
+
+// El metodo forEch no devuelve nada
+
+/* let dato = amigos.forEach(amigo => `Hola ${amigo}`)
+console.log(dato) // Tampoco devueve nada */
+
+// La finalidad de forEach es simplemente hacer algo con los elementos
