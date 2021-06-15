fde4f42619baef35b324fc3971bd4a2539f8ff92
Maggie-Sarmiento <gsmaggie001@gmail.com>
2021-06-15 11:00:55 -0500

    Class 5.6 Funciones como constantes

--- scripts.js
+++ scripts.js
@@ -1,28 +1,10 @@
 /**
- * FUNCIONES - FUNCIONES ANONIMAS
+ * FUNCIONES - FUNCIONES COMO CONSTANTES
  */
 
-
-// Almaceno una funcion en una variable sumar
-
-let sumar = function (a, b, c) {
-    return a + b + c
+const saludar = function(nombre) { 
+    console.log(`Hola ${nombre}`)
+//    return "jaja"
 }
 
-// Se ejecuta la funcion anonima porque se guardo en la variable sumar
-
-// console.log(sumar(4, 5, 5)) // podriamos imprimirla en cosola
-
-// o almacenarla en otra variable y posterior imprimirla ejecucion de sumar
-
-     // let resultado = sumar(2, 4, 5)  
-
-   // console.log(resultado) 
-
-// Las funciones anonimas tambien nos sevirian para ser autoinvocadas
-
-(function (a, b, c) {
-    console.log(a + b + c)
-} (2, 4, 5))
-
-// Estoy declarando la función y al mismo tiempo ejecutando
+console.log(saludar)
