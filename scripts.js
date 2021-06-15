71f78ec3ba1da8a6a5c176b4c75f2c38e05b261c
Maggie-Sarmiento <gsmaggie001@gmail.com>
2021-06-15 13:19:02 -0500

    Class 5.8 Funciones flecha

--- scripts.js
+++ scripts.js
@@ -1,46 +1,54 @@
 /**
- * SCOPE
+ * FUNCIONES FLECHA - arrow functions
+ *   =>
  */
 
-// El Scope es el alcance que tienen ciertos datos en este caso el de las variables
-/*  let nombre = "Alexys"
- nombre = "Juan"  // variable nombre tiene alcance global a fuera de la función
-
-function saludar(nombre) {
-    let nombre = "Beto"   // Esta variable solo existe dentro de la función
-    console.log(`Hola ${nombre}`)
+// funcion normal anonima almacenada en una constante
+/* const sumar = function(a, b) {
+    return a + b
 }
 
-saludar() */
-
-
-let nombre = "Alexys"
-nombre = "Juan"  // variable nombre tiene alcance global a fuera de la función
-
-const saludar = function() {
-   let nombre = "Beto"   // Esta variable solo existe dentro de la función y asi se deben de manejar
-   console.log(`Hola ${nombre}`)
+const restar = function(c, d) {
+    return c - d
 }
 
-saludar()
-console.log(nombre)
-
-// Cuando tenemos muchas funciones podemos estar adectando variables que no deberian
-// Las funciones deberian afectar solo variables que vivan dentro de la funcion
-
-// Mejor si no se usan las variables globales y en su lugar usar constantes
+console.log(sumar(10, 5))
+console.log(restar(5, 1)) */
 
 
-const NOMBRE = "Carlos"
 
-const saludo = function() {
-//    NOMBRE = "Pedro"  // error de asignacion de un valor a una constante
-    const NOMBRE = "Pedro"  // Esto es el scope, el valor que estoy declarando solo va a vivir dentro de la funcion
-    console.log(`Hola ${NOMBRE}`)
+// Aplicando las funciones flecha 
+const sumar = (a, b) => a + b    // Esta funcion esta almacenada en una constante que recibe dos parametros y retorna la suma de los mismos
+const restar = (c, d) => c - d
+
+console.log(sumar(15, 5))
+console.log(restar(10, 1))
+
+
+// Otra aplicación de las funciones flecha
+const saludar = nombre => `Hola ${nombre} buenos dias`  // Cuando las funciones flecha estan recibiendo solo un parametro omitimos los parentesis
+console.log(saludar("Maggie Sarmiento"))
+
+// const saludar = () => `Hola ${nombre} buenos dias` // Si nuestra funcion no necesita parametros si requiere parentesis, si es más de un parametro se pone entre parentesis y se separa con comas
+
+
+// como validar que un dato sea un string para determinada funcion flecha
+
+const saludar2 = nombre => {
+    if(typeof nombre === "string"){
+        return `Hola ${nombre} buenos dias`
+    } else {
+        console.error("Tipo de dato equivocado")
+    }
 }
 
-saludo()
-console.log(NOMBRE)
+console.log(saludar2(256))
 
-// en este caso como no se puede hacaer una reasignacion, no me preocupo, y se que cuando yo imprima NOMBRE  se que el valor no va a cambiar
+// Si necesito escribir codigo en una funcion flecha abro parentesis y ahi mismo escribir el return
+// Mientras mas pequeñas las funciones mejor
+// Si es una funcion muy grande buscar dividirlas en mas pequeñas
 
+// forma simplificada de validar una funcion flecha sencilla
+
+const saludar3 = nombre => typeof nombre === "string" ? `Hola ${nombre}` : "Paramentro no valido."
+console.log(saludar3(850))
