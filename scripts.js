eb81439eb4bbbf44b33f627fef1c9dfef9d805a8
Maggie-Sarmiento <gsmaggie001@gmail.com>
2021-06-15 10:42:57 -0500

    Class 5.5 Funciones anónimas

--- scripts.js
+++ scripts.js
@@ -1,61 +1,28 @@
 /**
- * FUNCIONES - FUNCIONES DE PRIMERA CLASE
+ * FUNCIONES - FUNCIONES ANONIMAS
  */
 
-// Funciones como ciudadano de primera clase: Término que suena 
-// totalmente exagerado para describir una característica de ciertos 
-// lenguajes de programación. Cuando algo puede ser ciudadano de primera 
-// clase, se refiere a que es un valor que puede ser asignado a variables, 
-// pasado como parámetro o devuelto como resultado en una función.
 
-// En lenguajes como Java, estos valores son los tipos de datos: int, 
-// double, String, Object, etc. Y como son de primera clase, no es raro 
-// verlos aplicados de esa manera en todas partes en el código: int x = 1 
-// x es una variable de tipo entero 
+// Almaceno una funcion en una variable sumar
 
-// Los lenguajes que permiten que las funciones sean de primera clase no 
-// tienen problema en que las funciones sean usadas justo como cualquier 
-// otro tipo de datos. como es el caso de C# y Javascript
-
-// En javascript al ser las funciones tratadas como ciudadanos de primera clase
-// pueden manipularse de la misma manera que una entidad como boolean como string 
-// como number las cuales a su vez tambien son entidades de primera clase
-
-function obtenerNombreCompleto(nombre, apellidos) {
-//    let nombreCompleto = `${nombre} ${apellidos}`
-//    return nombreCompleto
-    return `${nombre} ${apellidos}`
+let sumar = function (a, b, c) {
+    return a + b + c
 }
 
+// Se ejecuta la funcion anonima porque se guardo en la variable sumar
 
-/**
-* Entonces, una función se considera de primera clase porqué puede ser almacenada en 
-* una variable, enviada cómo argumento a otra función y ser devuelta por una función.
-*/
+// console.log(sumar(4, 5, 5)) // podriamos imprimirla en cosola
 
-function ejemplo() {
-    return "Soy una función de primera clase";
-   }
-   //1er punto. La almacenamos para luego ejecutarla con su nueva referencia:
-   let f_primera_clase = ejemplo;
-   console.log("1er punto: ", f_primera_clase());
-   
-   //2do punto. La enviamos como argumento a otra función.
-   function uso_otra_funcion(funcion) {
-    return funcion();
-   }
-   console.log("2do punto: ", uso_otra_funcion(ejemplo));
+// o almacenarla en otra variable y posterior imprimirla ejecucion de sumar
 
-let nombreCompleto = obtenerNombreCompleto("Carlos", 256)
+     // let resultado = sumar(2, 4, 5)  
 
-function primera_funcion() {
-    console.log("primera función")
-    return function ejemplo() {
-      return "Soy un función devuelta y definida dentro de otra";
-    };
-   }
-   f_primera_clase = primera_funcion();
-   console.log("3er punto: ", f_primera_clase());
+   // console.log(resultado) 
 
+// Las funciones anonimas tambien nos sevirian para ser autoinvocadas
 
+(function (a, b, c) {
+    console.log(a + b + c)
+} (2, 4, 5))
 
+// Estoy declarando la función y al mismo tiempo ejecutando
